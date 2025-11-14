97a7dbcbad93b9a2baac164ab2b7b1d057274247
Maksym Parandyk <128466050+pieroszek@users.noreply.github.com>
2025-11-15 00:57:02 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,46 @@
+#include "database.c"
+#include <stdio.h>
+
+
+#define MAX_INPUT 100
+
+void print_prompt(){
+	printf("db> ");
+}
+
+
+void handle_command(char * command){
+	if(strcmp(command, "create table") == 0) {
+		printf("creating table...\n");
+		create_table_flag = 1;
+	}
+	return;
+}
+
+
+
+int main(int argc, char *argv[]){
+	db_main();
+
+	char input[MAX_INPUT];
+    
+	if(0){ //only for compile
+	while (1) {
+		print_prompt();
+
+		// This BLOCKS until user presses Enter - which is FINE!
+		if (fgets(input, sizeof(input), stdin)) {
+			// Remove newline
+			input[strcspn(input, "\n")] = 0;
+
+			// Handle empty input
+			if (strlen(input) == 0) {
+				continue;
+			}
+
+			handle_command(input);
+		}
+	}
+	}
+	return 0;
+}
