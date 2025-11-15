a8322b8eb19a5e267b810a30d42910851eddefc3
Maksym Parandyk <128466050+pieroszek@users.noreply.github.com>
2025-11-16 01:08:22 +0800

    Update database.c

--- database.c
+++ database.c
@@ -22,7 +22,14 @@ void init_btrl(BTree_Root_List btrl){
 }
 
 
-void add_to_btrl(BTree_Root_List * btrl, BTree_Node * node);
+void add_to_btrl(BTree_Root_List * btrl, BTree_Node * node){
+	if(btrl->counter >= (btrl->len - 1)){
+		btrl->len += 2;
+		btrl->List = realloc(btrl->List, sizeof(BTree_Node) * btrl->len);
+	}
+	btrl->List[btrl->counter] = node;
+	btrl->counter++;
+}
 
 void init_fpl(FILE_PTR_LIST fpl){
 	fpl.file_ptr_list = malloc(sizeof(FILE*) * 10);
@@ -46,23 +53,33 @@ void init_tsl(TableSchema_List tsl){
 
 }
 
-void add_to_tsl(TableSchema_List * tsl, TableSchema * table_schema); 
+void add_to_tsl(TableSchema_List * tsl, TableSchema * table_schema){
+	if(tsl->counter >= (tsl->len -1)){
+		tsl->len += 2;
+		tsl->List = realloc(tsl->List, sizeof(TableSchema) * tsl->len);
+	}
+	tsl->List[tsl->counter] = table_schema;
+	tsl->counter++;
+}
 
-TableSchema * create_table(){
+void create_table(){
 	//im creating a new root node and adding it to the list,
 	//the table schema should have the same index as the root node
+	BTree_Node * new_root_node = create_btree_node();
+	add_to_btrl(&Root_Node_List, new_root_node);
 	
-
-	TableSchema * newtable = malloc(sizeof(newtable));
+	TableSchema * newtable = malloc(sizeof(*newtable));
 	newtable->table_name = "table";
 	newtable->root_node_offset = 0x0; //todo when file read write is implemented
-	newtable->column_count = 0;
-	return newtable;
+	newtable->column_count = 0x0;
+	add_to_tsl(&Table_Schema_List, newtable);
+	
+	printf("creating table...\n");
+	
 }
 
 
-
-int db_main () {
+int db_init_main() {
 	//list to keep track of files where data is saved	
 	init_fpl(TableSchema_File_Ptr_List);
 	init_fpl(TableData_File_Ptre_List);
@@ -73,6 +90,10 @@ int db_main () {
 	
 
 	create_table_flag = 0;
+	return 0;
+
+}
+int db_main () {
 
 	if(create_table_flag){
 		create_table_flag = 0;
