8fcf1de14cc65ea7331efe983a9bdfd127e4b45f
Maksym Parandyk <128466050+pieroszek@users.noreply.github.com>
2025-11-15 21:21:20 +0800

    Update database.h

--- database.h
+++ database.h
@@ -1,9 +1,22 @@
+#ifndef UGH_H
+#define UGH_H
+
 #include <stdlib.h>
+#include <stdio.h>
 #define ORDER 200
 //4kb == 4096
 
-size_t create_table_flag;
+int create_table_flag;
 size_t name_table_flag;
+
+typedef struct {
+	FILE ** file_ptr_list;
+	int counter;
+	int len;
+} FILE_PTR_LIST;
+
+void add_to_fpl(FILE_PTR_LIST fpl, FILE * file_ptr);
+void init_fpl(FILE_PTR_LIST fpl);
 
 typedef struct {
     char column_name[50];
@@ -29,19 +42,22 @@ typedef struct {
 
 
 typedef struct {
-	BTree_node ** List;
-	int root_list_counter;
-	int root_list_length; 
+	BTree_Node ** List;
+	int counter;
+	int len;
 } BTree_Root_List;
+
+void init_btrl(BTree_Root_List btrl);
+void add_to_btrl(BTree_Root_List * btrl, BTree_Node * node); 
 
 typedef struct {
 	TableSchema ** List;
-	int table_list_counter;
-	int table_list_length;
+	int counter;
+	int len;
 } TableSchema_List;
 
-
-
+void init_tsl(TableSchema_List tsl);
+void add_to_tsl(TableSchema_List * tsl, TableSchema * table_schema); 
 
 BTree_Node * create_btree_node();
 TableSchema * create_table();
