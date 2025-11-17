30201c472451bed2e373d3194b4323131cdc14b4
Maksym Parandyk <128466050+pieroszek@users.noreply.github.com>
2025-11-17 18:53:58 +0800

    Create file_io.c

---
+++ file_io.c
@@ -0,0 +1,20 @@
+#include "file_io.h"
+#include "database.h"
+// list to keep track of file pointers
+FILE_PTR_LIST TableSchema_File_Ptr_List;
+FILE_PTR_LIST TableData_File_Ptr_List;
+// "./data" where files stored
+
+void check_data_dir(){
+        system("mkdir -p data");
+}
+
+int create_btree_file(){
+        FILE * file = fopen("data/btree_file.data","ab+");     
+        add_to_fpl(&TableData_File_Ptr_List, file);
+       
+        if(!file) {
+                return 1;
+        }
+        return 0;
+}
