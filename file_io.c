5e40e71070f70826bd91cf29d74b6601c71dde6b
Maksym Parandyk <128466050+pieroszek@users.noreply.github.com>
2025-11-23 16:27:20 +0800

    Update file_io.c

--- file_io.c
+++ file_io.c
@@ -9,7 +9,7 @@ void check_data_dir(){
         system("mkdir -p data");
 }
 
-/*
+
 int create_btree_file(){
         FILE * file = fopen("data/btree_file.data","ab+");     
         add_to_fpl(&TableData_File_Ptr_List, file);
@@ -21,11 +21,11 @@ int create_btree_file(){
 }
 
 int create_ts_file(){
-        FILE * file = fopen("data/tableschema_file.data","ab+");
-        add_to_fpl(&TableSchema_File_Ptr_List, file);
-        if(!file) {
-                return 1;
-        }
+
+        
+       
+      
         return 0;
 }
-*/
+
+      
