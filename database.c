da6553831d9eb97f7303dd8e0b6826177e88d09f
Maksym Parandyk <128466050+pieroszek@users.noreply.github.com>
2025-11-26 23:23:10 +0800

    Update database.c

--- database.c
+++ database.c
@@ -1,8 +1,9 @@
 #include <string.h>
 #include "database.h"
 #include "file_io.c"
-
+//echo "database.c" | entr -r sh -c 'clear && gcc -std=c99 -Wall -Werror --save-temps -fsanitize=address main.c -o ughDB && ./ughDB'
 BTree_Root_List BTree_Ptr_List;
+TableSchema_List TableSchema_Ptr_List;
 
 void init_fpl(FILE_PTR_LIST * fpl){
 	fpl->counter = 0;
@@ -11,6 +12,12 @@ void init_fpl(FILE_PTR_LIST * fpl){
 	return;
 }
 
+void init_tsl(TableSchema_List * tsl){
+	tsl->len = 5;
+	tsl->counter = 0;
+	tsl->List = malloc(sizeof(TableSchema*) * tsl->len);
+
+}
 void init_btrl(BTree_Root_List * btrl){
 	btrl->counter = 0;
 	btrl->len = 5;
@@ -31,9 +38,12 @@ Column * init_col(char * id, col_type_enum type, int size, int offset){
 
 
 void init_table_schema(TableSchema * ts, char * table_name){
+	ts->column_count = 1;
+	ts->column_len = 5;
 	Column * key_col = init_col("key", 0, sizeof(int), 0);
 	ts->table_name = table_name;
-	ts->columns = key_col;
+	ts->columns = malloc(sizeof(Column*) * ts->column_len);
+	ts->columns[ts->column_count] = key_col;
 	return;
 }
 
@@ -56,6 +66,32 @@ void add_to_fpl(FILE_PTR_LIST * fpl, FILE * file_ptr){
 	return;
 }
 
+TableSchema* find_table_by_name(char * table_name, TableSchema_List * tsl){
+	if(tsl->counter == 0){
+		printf("no tables exist");
+		return 0x0; 
+	}
+	if(tsl->counter == 1 && tsl->List[0]->table_name == table_name){
+		return tsl->List[0];
+	}
+	for(int i = 0; i <= tsl->len; i++){
+		if(tsl->List[i]->table_name == table_name){
+			return tsl->List[i];
+		}
+	}
+	return 0x0;
+}
+
+void show_table(char * table_name){
+	TableSchema * table = find_table_by_name(table_name,&TableSchema_Ptr_List);
+	printf("\n%s\n", table_name);
+	for(int i = 0; i <= table->column_count; i++){
+		printf("%s ||", table->columns[i]->column_name);
+	}
+	show_table_flag = 0;
+	return;
+}
+
 void create_column(char * name, char * type, char * table_name){
 	create_column_flag = 0;
 	/* parse type */
@@ -64,20 +100,30 @@ void create_column(char * name, char * type, char * table_name){
 		type_param = 0;
 	}
 
+	/* get table from table name */
+	TableSchema * table = find_table_by_name(table_name, &TableSchema_Ptr_List);
+	if(!table){ 
+		return; //table not found
+	}
+
 	/* create column with the name */
-	Column * new_col = init_col(name, type_param, 1/*should be from table->column_counter */, 0);
+	Column * new_col = init_col(name, type_param,  table->column_count , 0);
 	if(!new_col){
 		return;
 	}
 	
 	/* modify table schema for the new column */
-	
-	/* get type now or with first append */
+	if(table->column_count == (table->column_len - 1)){ //check for bounds
+		return; // should add resize logic here 
+	}
+	table->column_count++;
+	table->columns[table->column_count] = new_col;
+
 	
 	return;
 }
 
-void create_table(){
+void create_table(char * table_name){
 	create_table_flag = 0;
 	printf("creating table...\n");
 	/* create table schema */
@@ -112,11 +158,14 @@ void create_table(){
 
 int db_main(){
 	if(create_table_flag){
-		create_table();
+		create_table(last_command);//last command hold table name from usr
 	}
 	if(create_column_flag){
 		create_column(saved_command,last_command,saved_command_2);
 		// params ( col name , col type , table to add col to ) 
+	}
+	if(show_table_flag){
+		show_table(last_command); //last command holds table name from usr
 	}
 	return 0;
 }
@@ -124,11 +173,12 @@ int db_main(){
 int db_init_main(){
 	create_table_flag = 0; //0 is false, 1 is triggers create_table()	
 	create_column_flag = 0; //0 is false, 2 and 1 are states
+	show_table_flag = 0; // 0 false, 1 see table
 
 	init_fpl(&TableSchema_File_Ptr_List);
 	init_fpl(&TableData_File_Ptr_List);
 
 	init_btrl(&BTree_Ptr_List);
-
+	init_tsl(&TableSchema_Ptr_List);
 	return 0;
 }
